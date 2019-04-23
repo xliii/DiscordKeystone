fe7abcfffb88ff1c6532380299e4d952570ef7a7
XLIII <xliii@mail.com>
2019-04-23 16:15:41 +0300

    Handle NaNs on /roll

--- app/command/Roll.ts
+++ app/command/Roll.ts
@@ -2,12 +2,27 @@ import {StringResolvable} from "discord.js";
 import {RollService} from "../service/RollService";
 
 module.exports = function(args: string[]): StringResolvable {
+    const usage = "Usage: **/keys roll [from] [to]**";
+
     if (args.length < 1) {
         return roll(1, 100);
     }
 
     let to = parseInt(args[0]);
-    return args.length == 1 ? roll(1, to) : roll(parseInt(args[1]), to);
+    if (isNaN(to)) {
+        return usage;
+    }
+
+    if (args.length == 1) {
+        return roll(1, to);
+    }
+
+    let from = parseInt(args[1]);
+    if (isNaN(from)) {
+        return usage;
+    }
+
+    return roll(from, to);
 
     function roll(from: number, to: number): StringResolvable {
         return new RollService().roll(from, to).toString();

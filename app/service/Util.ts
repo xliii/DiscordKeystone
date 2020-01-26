224dd1c89616112ed0722852a2101c7c47de4cd3
xliii <xliii@mail.com>
2020-01-26 21:04:49 +0200

    Disabled DM processing

--- app/service/Util.ts
+++ app/service/Util.ts
@@ -1,4 +1,4 @@
-import {Message, StringResolvable, TextChannel} from "discord.js";
+import {Client, DMChannel, Message, StringResolvable, TextChannel} from "discord.js";
 
 export function sendMessage(channel: TextChannel, message: StringResolvable) {
     console.log(`${channel.name} > ${message}`);
@@ -11,4 +11,14 @@ export function respond(message: Message, response: StringResolvable) {
 
 export function camelcase(str: string) {
     return str.toLowerCase().replace(/^\w/, c => c.toUpperCase());
+}
+
+export function processDM(client:Client, message:Message) {
+    const dmChannel: DMChannel = message.channel as DMChannel;
+    if (dmChannel.recipient.id == process.env.ADMIN) {
+
+        console.log(`${message.author.username} < ${message}`);
+        const channel: TextChannel = client.channels.get(process.env.CHANNEL_ID || '') as TextChannel;
+        sendMessage(channel, message.content);
+    }
 }

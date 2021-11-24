2cdc0aef4c455be035e2939a6ef42d2d1f5b02bd
xliii <xliii@mail.com>
2021-11-24 22:27:12 +0200

    Fixed intents, message event handler, use guild application commands, extracted interactions

--- app/register_command/DeleteCommands.ts
+++ app/register_command/DeleteCommands.ts
@@ -7,15 +7,16 @@ function deleteCommand() {
 
     const token = process.env.BOT_TOKEN;
     const clientId = '558257059276128257';
+    const guildId = '327478651337834496';
 
     const rest = new REST({version: '9'}).setToken(token);
 
-    rest.get(Routes.applicationCommands(clientId))
+    rest.get(Routes.applicationGuildCommands(clientId, guildId))
         .then((data: any) => {
             console.log(data);
             const promises = [];
             for (const command of data) {
-                const deleteUrl = `${Routes.applicationCommands(clientId)}/${command.id}`;
+                const deleteUrl = `${Routes.applicationGuildCommands(clientId, guildId)}/${command.id}`;
                 promises.push(rest.delete(deleteUrl));
                 console.log("Delete: " + command.name);
             }

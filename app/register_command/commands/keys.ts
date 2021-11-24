9df55a1d78fb13e6701af30f61175f86b0d4bdd4
xliii <xliii@mail.com>
2021-11-25 00:31:06 +0200

    /keys add

--- app/register_command/commands/keys.ts
+++ app/register_command/commands/keys.ts
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const {SlashCommandBuilder} = require('@discordjs/builders');
 
 const data = new SlashCommandBuilder()
     .setName('keys')
@@ -7,8 +7,30 @@ const data = new SlashCommandBuilder()
         return subcommand
             .setName('list')
             .setDescription('List keystones')
+    }).addSubcommand((subcommand: any) => {
+        return subcommand
+            .setName('add')
+            .setDescription('Add keystone')
+            .addStringOption((option: any) => {
+                return option
+                    .setName('dungeon')
+                    .setDescription('Dungeon')
+                    .setRequired(true)
+                    .addChoice('Necrotic Wake', 'nw')
+                    .addChoice('De Other Side', 'dos')
+                    .addChoice('Plaguefall', 'pf')
+                    .addChoice('Halls of Atonement', 'hoa')
+                    .addChoice('Mists of Tirna Scithe', 'mots')
+                    .addChoice('Sanguine Depths', 'sd')
+                    .addChoice('Spires of Ascension', 'soa')
+                    .addChoice('Theater of Pain', 'top')
+            })
+            .addIntegerOption((option: any) => {
+                return option
+                    .setName('level')
+                    .setDescription('Keystone Level')
+                    .setRequired(true)
+
+            })
     });
-
-console.log(data);
-
 export default data.toJSON();

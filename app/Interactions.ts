22282212be949a1c603861f0fff6d6f7f9f3d720
xliii <xliii@mail.com>
2021-11-24 23:30:17 +0200

    keys list added

--- app/Interactions.ts
+++ app/Interactions.ts
@@ -1,13 +1,43 @@
 import {Interaction} from "discord.js"
+import repositories from "./repository/Repositories";
+
+const keystoneRepository = repositories.keystoneRepository();
+
+function processList(options: any): Promise<any> {
+    return keystoneRepository.List().then(keystones => {
+        if (keystones.length == 0) {
+            return "No keystones available";
+        }
+
+        let result = [];
+        result.push('All keystones:');
+        result.push(...keystones);
+        return result;
+    });
+}
+
+function processResponse(options: any): Promise<any> {
+    switch (options.getSubcommand()) {
+        case 'list': {
+            return processList(options);
+        }
+        default: {
+            return Promise.resolve('Unknown command');
+        }
+    }
+}
 
 module.exports = function (interaction: Interaction): void {
     console.log(interaction);
     if (!interaction.isCommand()) return;
 
-    const {commandName} = interaction;
+    const {commandName, options} = interaction;
 
-    console.log(commandName);
-    interaction.reply(commandName).then(result => {
+    if (commandName != 'keys') return;
+
+    processResponse(options).then(response => {
+        return interaction.reply(response);
+    }).then(result => {
         console.log(result);
     })
 };

0862fc5de8160b069917620cf6d61c9ffe436369
xliii <xliii@mail.com>
2021-11-25 14:15:46 +0200

    /keys neko/ragequit

--- app/modules/General.ts
+++ app/modules/General.ts
@@ -2,6 +2,9 @@ const processKeystones = require("./Keystones");
 const processRoll = require("./Roll");
 const processAffixes = require("./Affixes");
 
+import RagequitService from "../service/RagequitService";
+import NekoService from "../service/NekoService";
+
 function coffee(): Promise<any> {
     return Promise.resolve("IDI NAHUJ");
 }
@@ -10,9 +13,24 @@ function tea(): Promise<any> {
     return Promise.resolve("https://i.ytimg.com/vi/755BDwzxv5c/hqdefault.jpg");
 }
 
+function ragequit(): Promise<any> {
+    return RagequitService.getCounter().then(counter => {
+            return `Days since last ragequit: ${counter}`;
+        }
+    )
+}
+
+function neko(): Promise<any> {
+    return NekoService.getNeko();
+}
+
 module.exports = function processCommand(options: any, user: any): Promise<any> {
     const command = options.getSubcommand();
     switch (command) {
+        case 'neko':
+            return neko();
+        case 'ragequit':
+            return ragequit();
         case 'tea':
             return tea();
         case 'coffee':

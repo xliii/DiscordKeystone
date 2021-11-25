c0ca2e5fad6e8d3b9da792e4017b9345b1c9488e
xliii <xliii@mail.com>
2021-11-25 14:02:25 +0200

    /keys roll migrated

--- app/modules/Affixes.ts
+++ app/modules/Affixes.ts
@@ -1,11 +1,5 @@
 import AffixesService from "../service/AffixesService";
 
 module.exports = function processCommand(options: any, user: any): Promise<any> {
-    const command = options.getSubcommand();
-    switch (command) {
-        case 'affixes':
-            return AffixesService.getAffixes();
-        default:
-            return Promise.resolve('Unknown command');
-    }
+    return AffixesService.getAffixes();
 };
--- app/modules/General.ts
+++ app/modules/General.ts
@@ -1,10 +1,12 @@
-
 const processKeystones = require("./Keystones");
+const processRoll = require("./Roll");
 const processAffixes = require("./Affixes");
 
 module.exports = function processCommand(options: any, user: any): Promise<any> {
     const command = options.getSubcommand();
     switch (command) {
+        case 'roll':
+            return processRoll(options, user);
         case 'affixes':
             return processAffixes(options, user);
         default:

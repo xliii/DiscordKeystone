39540ad956ab5c0a2902c74fc3159c5826f227d7
xliii <xliii@mail.com>
2021-11-25 13:29:54 +0200

    Alias commands migration done

--- app/modules/Alias.ts
+++ app/modules/Alias.ts
@@ -1,8 +1,20 @@
 import repositories from "../repository/Repositories";
+import {camelcase} from "../service/Util";
+import {Alias} from "../model/Alias";
 
 const aliasRepo = repositories.aliasRepository();
 
-function processList(options: any, user: any): Promise<any> {
+function extractAlias(user: any): Promise<string> {
+    const discordId: string = user.id;
+    const discordName: string = user.username;
+    return aliasRepo.Get(discordId)
+        .then(alias => alias.character)
+        .catch(() => {
+            throw `No alias found for **${discordName}**`
+        });
+}
+
+function list(options: any, user: any): Promise<any> {
     return aliasRepo.List().then(aliases => {
         return aliases.length > 0 ?
             aliases.join('\n') :
@@ -10,11 +22,43 @@ function processList(options: any, user: any): Promise<any> {
     });
 }
 
+function set(options: any, user: any): Promise<any> {
+    const discordId: string = user.id;
+    const discordName: string = user.username;
+    const character = camelcase(options.getString('character'));
+
+    return aliasRepo.Add(new Alias(discordName, discordId, character)).then(() => {
+        return `Alias **${character}** added for **${discordName}**`;
+    });
+}
+
+function get(options: any, user: any): Promise<any> {
+    return extractAlias(user).then(character => {
+        return `**${user.username}**'s alias is **${character}**`;
+    }).catch(error => error);
+}
+
+function remove(options: any, user: any): Promise<any> {
+    const discordId: string = user.id;
+    const discordName: string = user.username;
+    return aliasRepo.Remove(discordId).then(alias => {
+        return alias ?
+            `Alias **${alias.character}** removed from **${discordName}**` :
+            `No alias found for **${discordName}**`;
+    })
+}
+
 module.exports = function processCommand(options: any, user: any): Promise<any> {
     const command = options.getSubcommand();
     switch (command) {
         case 'list':
-            return processList(options, user);
+            return list(options, user);
+        case 'get':
+            return get(options, user);
+        case 'set':
+            return set(options, user);
+        case 'remove':
+            return remove(options, user);
         default:
             return Promise.resolve('Unknown command');
     }
